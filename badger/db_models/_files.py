7f99bd03f336941b365baa9b524df8ba5a16af3b
pidi3000 <pidi3000@live.com>
2024-01-08 17:21:19 +0100

    added db fields for download handler Added table 'Download_Queue' Added column 'dl_status' to File tables

--- badger/db_models/_files.py
+++ badger/db_models/_files.py
@@ -1,10 +1,39 @@
+from __future__ import annotations
+
 from badger.extension import db
 from badger.db_models import _Base_Mixin
 
 from sqlalchemy.ext.hybrid import hybrid_property
 
 
-class Audio_File(_Base_Mixin, db.Model):
+class Download_Queue(_Base_Mixin, db.Model):
+    __tablename__ = 'download_queue'
+
+    id = db.Column(db.Integer, db.ForeignKey(
+        'song_meta_data.id'), primary_key=True)
+    task_id: str = db.Column(db.String(200), nullable=False)
+
+    @classmethod
+    def create(cls, song_id: int, task_id: str) -> Download_Queue:
+        return super().create(id=song_id, task_id=task_id)
+
+    @classmethod
+    def get(cls, song_id: int = None, task_id: str = None) -> Download_Queue | None:
+
+        if isinstance(song_id, int):
+            return cls.get_by_ID(song_id)
+
+        if isinstance(task_id, str):
+            return cls.get_by_Task_ID(task_id)
+
+        raise ValueError("Either song_id or task_id must be set")
+
+    @classmethod
+    def get_by_Task_ID(cls, task_id: str) -> Download_Queue | None:
+        return Download_Queue.query.filter_by(task_id=task_id).firts()
+
+
+class Audio_File(db.Model):
     __tablename__ = 'audio_file'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -12,18 +41,17 @@ class Audio_File(_Base_Mixin, db.Model):
     location = db.Column(db.String(200), nullable=False)
     name = db.Column(db.String(200), nullable=False)
     extension = db.Column(db.String(200), nullable=False)
-    
+
+    dl_status = db.Column(db.String(200), nullable=False, default="pending")
+
     audio_length = db.Column(db.Integer)
 
-    def __repr__(self):
-        return f'<{self.__class__} {self.name}>'
-
     @classmethod
-    def create(cls, location, name, extension, audio_length) -> 'Audio_File':
+    def create(cls, location, name, extension, audio_length) -> Audio_File:
         return super().create(location, name, extension, audio_length)
 
 
-class Image_File(_Base_Mixin, db.Model):
+class Image_File(db.Model):
     __tablename__ = 'image_file'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -32,9 +60,8 @@ class Image_File(_Base_Mixin, db.Model):
     name = db.Column(db.String(200), nullable=False)
     extension = db.Column(db.String(200), nullable=False)
 
-    def __repr__(self):
-        return f'<{self.__class__} {self.name}>'
+    dl_status = db.Column(db.String(200), nullable=False, default="pending")
 
     @classmethod
-    def create(cls, location, name, extension) -> 'Image_File':
+    def create(cls, location, name, extension) -> Image_File:
         return super().create(location, name, extension)
